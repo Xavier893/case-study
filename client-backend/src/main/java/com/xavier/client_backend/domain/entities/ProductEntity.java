6a1af6de915f9a021bcbc85b01ce41dc53e556e2
Xavier893 <xavierrynolds@gmail.com>
2024-07-08 15:24:36 +0200

    Added Product Entity (lombok used for boilerplate code)

---
+++ client-backend/src/main/java/com/xavier/client_backend/domain/entities/ProductEntity.java
@@ -0,0 +1,18 @@
+package com.xavier.client_backend.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "products")
+public class ProductEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Float price;
+}
