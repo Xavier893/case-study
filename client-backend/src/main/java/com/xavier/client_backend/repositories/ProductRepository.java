c5c099d21c45ba1b9f8a83a498213d84d6bae637
Xavier893 <xavierrynolds@gmail.com>
2024-07-13 23:38:57 +0200

    chore: Add @Repository annotation to ProductRepository

--- client-backend/src/main/java/com/xavier/client_backend/repositories/ProductRepository.java
+++ client-backend/src/main/java/com/xavier/client_backend/repositories/ProductRepository.java
@@ -2,6 +2,8 @@ package com.xavier.client_backend.repositories;
 
 import com.xavier.client_backend.domain.entities.ProductEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends CrudRepository<ProductEntity, Long> {
 }
