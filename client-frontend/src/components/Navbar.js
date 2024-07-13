052713da8d436cfca865974209c66f9eba72c3a5
Xavier893 <xavierrynolds@gmail.com>
2024-07-13 23:41:48 +0200

    chore: Add Navbar component for navigation functionality

---
+++ client-frontend/src/components/Navbar.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const NavbarComponent = ({ isAuthenticated, client }) => {
+	const navigate = useNavigate();
+
+	const handleLogout = async () => {
+		try {
+			const response = await fetch(`${process.env.REACT_APP_API_URL}/logout`, {
+				method: "POST",
+				credentials: "include",
+				headers: {
+					"Content-Type": "application/json",
+				},
+			});
+			if (response.ok) {
+				navigate("/");
+				window.location.reload();
+			} else {
+				console.error("Logout failed");
+			}
+		} catch (error) {
+			console.error("Logout failed", error);
+		}
+	};
+
+	const handleLogin = () => {
+		navigate("/login");
+	};
+
+	return (
+		<nav className="navbar navbar-light bg-body-secondary">
+			<div className="container d-flex justify-content-between align-items-center">
+				<Link className="navbar-brand" to="/">
+					Home
+				</Link>
+				<div className="d-flex align-items-center">
+					<ul className="navbar-nav me-auto mb-2 mb-lg-0 d-flex align-items-center">
+						<li className="nav-item me-3">
+							<Link className="nav-link" to="/orders">
+								Orders
+							</Link>
+						</li>
+					</ul>
+					{isAuthenticated ? (
+						<button className="btn btn-outline-danger" onClick={handleLogout}>
+							Logout
+						</button>
+					) : (
+						<button className="btn btn-outline-success" onClick={handleLogin}>
+							Login
+						</button>
+					)}
+				</div>
+			</div>
+		</nav>
+	);
+};
+
+export default NavbarComponent;
