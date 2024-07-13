dfc7b17806beee4647fe2ea7d3fc21743c7a0f13
Xavier893 <xavierrynolds@gmail.com>
2024-07-13 23:39:20 +0200

    chore: Refactor ProductService interface and add getProductById method

--- client-backend/src/main/java/com/xavier/client_backend/services/ProductService.java
+++ client-backend/src/main/java/com/xavier/client_backend/services/ProductService.java
@@ -1,11 +1,10 @@
 package com.xavier.client_backend.services;
 
 import com.xavier.client_backend.domain.entities.ProductEntity;
-
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductService {
-    ProductEntity save(ProductEntity productEntity);
-
     List<ProductEntity> findAll();
+    Optional<ProductEntity> getProductById(Long id);
 }
