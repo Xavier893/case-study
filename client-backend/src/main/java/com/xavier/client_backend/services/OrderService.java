8d7f16b42bcd209f26a775f5f5429809c16be269
Xavier893 <xavierrynolds@gmail.com>
2024-07-14 20:04:29 +0200

    Changed OrderService Interface to work with CompletableFutures for Async communication

--- client-backend/src/main/java/com/xavier/client_backend/services/OrderService.java
+++ client-backend/src/main/java/com/xavier/client_backend/services/OrderService.java
@@ -3,12 +3,13 @@ package com.xavier.client_backend.services;
 import com.xavier.client_backend.domain.entities.OrderEntity;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 public interface OrderService {
-    OrderEntity save(OrderEntity orderEntity);
-    OrderEntity updateOrder(Long orderId, OrderEntity updatedOrder);
-    List<OrderEntity> findAll();
-    OrderEntity findById(Long id);
-    List<OrderEntity> findByClientId(Long clientId);
-    void deleteById(Long id);
+    CompletableFuture<OrderEntity> save(OrderEntity orderEntity);
+    CompletableFuture<OrderEntity> updateOrder(Long orderId, OrderEntity updatedOrder);
+    CompletableFuture<List<OrderEntity>> findAll();
+    CompletableFuture<OrderEntity> findById(Long id);
+    CompletableFuture<List<OrderEntity>> findByClientId(Long clientId);
+    CompletableFuture<Void> deleteById(Long id);
 }
