777f490bfbee9b35eb0a37f37fc97cafe1cf2a98
Xavier893 <xavierrynolds@gmail.com>
2024-07-16 16:28:02 +0200

    Update allowed origins in SecurityConfig.java

--- client-backend/src/main/java/com/xavier/client_backend/config/SecurityConfig.java
+++ client-backend/src/main/java/com/xavier/client_backend/config/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowCredentials(true);
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://mango-plant-0dc82e003.5.azurestaticapps.net/"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://mango-plant-0dc82e003.5.azurestaticapps.net"));
         configuration.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setExposedHeaders(Arrays.asList("Set-Cookie"));
